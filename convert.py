670af7dea53c2fa32ceafc5190cf6d19ba737282
huangzhengxiang <huangzhengxiang@sjtu.edu.com>
2023-06-06 16:04:54 +0800

    renew video

---
+++ convert.py
@@ -0,0 +1,14 @@
+import imageio 
+import cv2
+
+video_path="video\Ant-v2\DDPG.mp4"
+content=imageio.get_reader(video_path,"MP4")
+frames = []
+for j in range(600):
+    try:
+        frame = content.get_next_data()
+        frame = cv2.resize(frame,(800,400))
+        frames.append(frame)
+    except IndexError:
+        break
+imageio.mimwrite(video_path.replace("mp4","gif"),frames,"GIF")
